e336c93fdc291d17c3b391ed47181ffe54813879
Ross Boyd <rssbyd@outlook.com>
2021-12-08 23:09:50 +0000

    Day08
    Day08
    
    - Improve when sort happens, unknown list
    - Remove swap helper function to set intersection

--- day08/main.py
+++ day08/main.py
@@ -3,17 +3,6 @@
 """
 
 
-def inner_pattern(list1, list2) -> bool:
-    """
-    Returns True if all elements of list1 exist in list2, False otherwise
-    """
-    for el in list1:
-        if el not in list2:
-            return False
-
-    return True
-
-
 def solution(signal_notes: list) -> tuple:
     """
     returns tuple (part1, part2)
@@ -23,40 +12,37 @@ def solution(signal_notes: list) -> tuple:
     counter = 0
     output_sum = 0
     for note in signal_notes:
-        signal_patterns, output_value = note.split(
-            " | ")[0], note.split(" | ")[1]
+        signal_patterns, output_values = note.split(
+            " | ")[0], [note.split(" | ")[1]]
+        signal_patterns = ["".join(sorted(s))
+                           for s in signal_patterns.split(" ")]
+        output_values = ["".join(sorted(v))
+                         for v in output_values[0].split(" ")]
 
-        crib_sheet = {0: "", 1: "", 2: "", 3: "", 4: "",
-                      5: "", 6: "", 7: "", 8: "", 9: ""}
-
-        signal_patterns = signal_patterns.split(" ")
-        unknown_signal_patterns = signal_patterns.copy()
+        crib_sheet = {}
+        unknown_signal_patterns = []
 
         for signal in signal_patterns:
+            # Check if the digit is known from the length of the signal
             if len(signal) in [2, 3, 4, 7]:
                 match len(signal):
                     case 2:
                         crib_sheet[1] = signal
-                        unknown_signal_patterns.remove(signal)
-
                     case 3:
                         crib_sheet[7] = signal
-                        unknown_signal_patterns.remove(signal)
-
                     case 4:
                         crib_sheet[4] = signal
-                        unknown_signal_patterns.remove(signal)
-
                     case 7:
                         crib_sheet[8] = signal
-                        unknown_signal_patterns.remove(signal)
+            else:
+                unknown_signal_patterns.append(signal)
 
         for signal in unknown_signal_patterns:
 
             # All remaining signals are 2,3,5 or 0,6,9
             if len(signal) == 5:
                 # Signal is 2,3 or 5
-                if inner_pattern(crib_sheet[7], signal):
+                if len(list(set(crib_sheet[7]).intersection(signal))) == 3:
                     crib_sheet[3] = signal
                 else:
                     # 2 or 5
@@ -66,11 +52,11 @@ def solution(signal_notes: list) -> tuple:
                         crib_sheet[2] = signal
             elif len(signal) == 6:
                 # Signal is 0, 6 or 9
-                if inner_pattern(crib_sheet[4], signal):
+                if len(list(set(crib_sheet[4]).intersection(signal))) == 4:
                     crib_sheet[9] = signal
                 else:
                     # 0 or 6
-                    if inner_pattern(crib_sheet[7], signal):
+                    if len(list(set(crib_sheet[7]).intersection(signal))) == 3:
                         crib_sheet[0] = signal
                     else:
                         crib_sheet[6] = signal
@@ -78,14 +64,13 @@ def solution(signal_notes: list) -> tuple:
                 return 0, 0
 
         # Reverse the crib_sheet to pattern: digit, and sort the patterns for easier matching
-        crib_sheet = {"".join(sorted(v)): k for k, v in crib_sheet.items()}
+        crib_sheet = {v: k for k, v in crib_sheet.items()}
 
         # Now, go through the output, lookup the combinations in the crib sheet, and add to sum
         output_digits = ""
-        for value in output_value.split(" "):
-            crib = "".join(sorted(value))
-            output_digits += str(crib_sheet[crib])
-            if crib_sheet[crib] in [1, 4, 7, 8]:
+        for value in output_values:
+            output_digits += str(crib_sheet[value])
+            if crib_sheet[value] in [1, 4, 7, 8]:
                 counter += 1
 
         output_sum += int(output_digits)
